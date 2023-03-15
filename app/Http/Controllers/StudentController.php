f7d7fcc6a724d28cc19a1746bd4ba9f038af18d4
Jackietruong2805 <truonghao2805@gmail.com>
2023-03-15 08:44:29 +0700

    completed insert into DB with raw SQL

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function add(){
-       $student = new Student();
-       $student->student_name = 'Gia Hao';
-       $student->student_email = 'giahao@gmail.com';
-       $student->save();
+       DB::insert('insert into stdnt (student_name, student_email) values(?, ?)', ['hao', 'hao@gmail.com']);
     }
 }
